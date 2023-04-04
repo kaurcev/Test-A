8fe8c08db68a4f83e5812c38523597d374bc2604
kaurcev <skaurcev2004@yandex.ru>
2023-04-04 15:47:22 +0600

    fixed

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-// Dependencies.
 var express = require('express');
 var http = require('http');
 var path = require('path');
 var socketIO = require('socket.io');
-
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
